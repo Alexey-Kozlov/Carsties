2ae2f67f3a97379428c8ea897ba2565a6a4d8068
Alexey-Kozlov <alexeytula@gmail.com>
2024-01-20 15:48:35 +0300

    Auction api ready

--- src/AuctionService/Controllers/AuctionController.cs
+++ src/AuctionService/Controllers/AuctionController.cs
@@ -53,4 +53,33 @@ public class AuctionController : ControllerBase
         if(!result) return BadRequest("Error save auction");
         return CreatedAtAction(nameof(GetAuctionById), new {auction.Id}, _mapper.Map<AuctionDTO>(auction));
     }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDTO updateAuctionDTO)
+    {
+        var auction = await _context.Auctions.Include(p => p.Item)
+            .FirstOrDefaultAsync(p => p.Id == id);
+        if(auction == null) return BadRequest("Запись не найдена");
+        auction.Item.Make = updateAuctionDTO.Make ?? auction.Item.Make;
+        auction.Item.Model = updateAuctionDTO.Model ?? auction.Item.Model;
+        auction.Item.Color = updateAuctionDTO.Color ?? auction.Item.Color;
+        auction.Item.Mileage = updateAuctionDTO.Mileage ?? auction.Item.Mileage;
+        auction.Item.Year = updateAuctionDTO.Year ?? auction.Item.Year;
+
+        var result = await _context.SaveChangesAsync() > 0;
+        if(result) return Ok();
+        return BadRequest("Ошибка обновления записи");
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteAuction(Guid id)
+    {
+        var auction = await _context.Auctions.FindAsync(id);
+        if(auction == null) return BadRequest("Запись не найдена");
+        _context.Auctions.Remove(auction);
+        var result = await _context.SaveChangesAsync() > 0;
+        if(result) return Ok();
+        return BadRequest("Ошибка удаления записи");
+    }
+
 }
--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c40d4033491b3bc9e88cf7b039b47f03d0dace78")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
